4995480be8ab7a23449ea0c5c70df52faf8cbdca
Pradnyesh Sawant <spradnyesh@gmail.com>
2015-03-22 12:54:17 +0530

    initialize

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-ml "0.1.0-SNAPSHOT"
+  :description "['Machine Learning' @ 'coursera' --Andrew Ng] using clojure + incanter"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter "1.5.5"]])
